463cffbccf0b65d7cf721ca53adf98db7ec12bc4
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-05 20:43:02 +0300

    Updated User table

--- Entities/User.cs
+++ Entities/User.cs
@@ -8,20 +8,16 @@ namespace File_Manager.Entities;
 public partial class User
 {
     public int UserId { get; set; }
-
     public string Username { get; set; }
-
     public string Password { get; set; }
-
     public string FirstName { get; set; }
-
     public string LastName { get; set; }
-
     public string Email { get; set; }
-
     public int DepartmentId { get; set; }
+    public string ImagePath { get; set; }
 
     public virtual Department Department { get; set; }
-
     public virtual ICollection<File> Files { get; set; } = new List<File>();
+    public virtual ICollection<ChatParticipant> ChatParticipants { get; set; } = new List<ChatParticipant>();
+    public virtual ICollection<Message> Messages { get; set; } = new List<Message>();
 }
