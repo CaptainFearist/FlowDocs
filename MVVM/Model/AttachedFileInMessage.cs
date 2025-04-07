dfd6f199d756cbf63e5499ecf452256db4b6e878
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-07 22:57:11 +0300

    Model of the file attached to the message

---
+++ MVVM/Model/AttachedFileInMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace File_Manager.MVVM.Model
+{
+    public class AttachedFileInMessage
+    {
+        public int AttachmentId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public int FileId { get; set; }
+    }
+}
