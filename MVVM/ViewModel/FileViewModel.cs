393320cd677a9d55d1d6e85524527d9e1ede8585
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-07 22:55:47 +0300

    New ViewModel for displaying files

---
+++ MVVM/ViewModel/FileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace File_Manager.MVVM.ViewModel
+{
+    public class FileViewModel
+    {
+        public int FileId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; internal set; }
+    }
+}
