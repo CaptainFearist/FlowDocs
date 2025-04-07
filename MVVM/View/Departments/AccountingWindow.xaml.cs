fc349fbbf6af4709d8d076da88670c9992cf54e3
Igor <114694615+CaptainFearist@users.noreply.github.com>
2025-04-07 16:42:10 +0300

    Removed unnecessary using

--- MVVM/View/Departments/AccountingWindow.xaml.cs
+++ MVVM/View/Departments/AccountingWindow.xaml.cs
@@ -4,7 +4,6 @@ using File_Manager.MVVM.View.Technician;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Win32;
 using System.Windows;
-using System.Diagnostics;
 using System.Windows.Controls;
 using System.Windows.Input;
 using File_Manager.MVVM.View.Messenger;
