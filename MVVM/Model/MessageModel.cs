dce92a61bbb2526835d7554e97cfdb71b24fd461
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-07 16:03:27 +0300

    Model added

---
+++ MVVM/Model/MessageModel.cs
@@ -0,0 +1,11 @@
+﻿namespace File_Manager.MVVM.Model
+{
+    public class MessageModel
+    {
+        public string SenderName { get; set; }
+        public string Content { get; set; }
+        public DateTime SentDate { get; set; }
+        public bool IsSenderCurrentUser { get; set; }
+    }
+
+}
