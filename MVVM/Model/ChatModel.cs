ecd93e94f3b501ec3f013cd2935b2ebedce0c74d
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-07 16:03:13 +0300

    Updated model

--- MVVM/Model/ChatModel.cs
+++ MVVM/Model/ChatModel.cs
@@ -1,10 +1,14 @@
-﻿namespace File_Manager.MVVM.Model
+﻿using System.Collections.ObjectModel;
+
+namespace File_Manager.MVVM.Model
 {
     public class ChatModel
     {
-        public int UserId { get; set; }
+        public int ChatId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string ImagePath { get; set; }
+
+        public ObservableCollection<MessageModel> Messages { get; set; } = new ObservableCollection<MessageModel>();
     }
 }
