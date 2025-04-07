cf7f3ae0113d9b135c6dc5320d4a1d11ce72d08f
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-07 16:28:18 +0300

    Converter for determining chat position

---
+++ Core/Services/BoolToAlignmentConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace File_Manager.Core.Services
+{
+    public class BoolToAlignmentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isCurrentUser && isCurrentUser)
+            {
+                return HorizontalAlignment.Right; // Для текущего пользователя
+            }
+            return HorizontalAlignment.Left; // Для других пользователей
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
