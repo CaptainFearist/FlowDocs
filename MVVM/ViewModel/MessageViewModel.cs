e2c96b194f433778249232beff32738b23a7f7f5
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-07 15:50:29 +0300

    Added a variable

---
+++ MVVM/ViewModel/MessageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace File_Manager.MVVM.ViewModel
+{
+    public class MessageViewModel
+    {
+        public string SenderName { get; set; }
+        public string Content { get; set; }
+        public DateTime SentDate { get; set; }
+
+        public bool IsOwnMessage { get; set; }
+    }
+
+}
