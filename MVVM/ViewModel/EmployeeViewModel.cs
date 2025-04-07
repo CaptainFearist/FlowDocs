b31c0a29ff9070f1a36dd74402b570c83e5dc58d
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-07 15:50:50 +0300

    Added variables

--- MVVM/ViewModel/EmployeeViewModel.cs
+++ MVVM/ViewModel/EmployeeViewModel.cs
@@ -2,9 +2,11 @@
 {
     public class EmployeeViewModel
     {
+        public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string DepartmentName { get; set; }
+        public string ImagePath { get; set; }
     }
 }
