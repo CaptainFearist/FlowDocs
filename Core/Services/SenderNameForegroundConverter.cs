f085c1416419b5d533499d8172a1eba0af629a02
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-09 20:46:14 +0300

    Add SenderNameForegroundConverter based on user

---
+++ Core/Services/SenderNameForegroundConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace File_Manager.Core.Services
+{
+    public class SenderNameForegroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isCurrentUser)
+            {
+                return isCurrentUser ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Color.FromRgb(0, 96, 169));
+            }
+            return new SolidColorBrush(Color.FromRgb(0, 96, 169));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
