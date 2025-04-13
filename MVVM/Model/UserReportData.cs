ff24bba3f0c50d40d8d441cc0fc938e9739a46fd
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-13 17:27:42 +0300

    Add UserReportData model

---
+++ MVVM/Model/UserReportData.cs
@@ -0,0 +1,9 @@
+﻿namespace File_Manager.MVVM.Model
+{
+    public class UserReportData
+    {
+        public string FullName { get; set; }
+        public string Department { get; set; }
+        public int FileCount { get; set; }
+    }
+}
