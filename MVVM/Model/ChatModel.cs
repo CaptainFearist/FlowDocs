e227a58b477df2a97dadac3ca1f716fb33f24a4a
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-17 01:13:47 +0300

    BitmapImage loading in ChatModel implemented

--- MVVM/Model/ChatModel.cs
+++ MVVM/Model/ChatModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+using System.IO;
 
 namespace File_Manager.MVVM.Model
 {
@@ -7,7 +9,40 @@ namespace File_Manager.MVVM.Model
         public int ChatId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string ImagePath { get; set; }
+        public BitmapImage ProfileImage { get; set; }
+
+        public byte[] ImagePath
+        {
+            get => null;
+            set
+            {
+                if (value != null && value.Length > 0)
+                {
+                    try
+                    {
+                        using (var memoryStream = new MemoryStream(value))
+                        {
+                            ProfileImage = new BitmapImage();
+                            ProfileImage.BeginInit();
+                            ProfileImage.CacheOption = BitmapCacheOption.OnLoad;
+                            ProfileImage.StreamSource = memoryStream;
+                            ProfileImage.EndInit();
+                            ProfileImage.Freeze();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ProfileImage = new BitmapImage(new Uri("pack://application:,,,/Images/default_user.png"));
+                        ProfileImage.Freeze();
+                    }
+                }
+                else
+                {
+                    ProfileImage = new BitmapImage(new Uri("pack://application:,,,/Images/default_user.png"));
+                    ProfileImage.Freeze();
+                }
+            }
+        }
 
         public ObservableCollection<MessageModel> Messages { get; set; } = new ObservableCollection<MessageModel>();
     }
