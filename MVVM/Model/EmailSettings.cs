e82ac6191760cd9c3931c4fdd3b38814235828f6
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-16 14:48:23 +0300

    Created EmailSettings model for mail configuration

---
+++ MVVM/Model/EmailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace File_Manager.MVVM.Model
+{
+    public class EmailSettings
+    {
+        public string SenderEmail { get; set; }
+        public string SenderName { get; set; }
+        public string SenderPassword { get; set; }
+        public string SmtpHost { get; set; }
+        public int SmtpPort { get; set; }
+    }
+}
