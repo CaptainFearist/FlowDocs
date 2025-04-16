fcb25d8395804b328689ebdbc0bdb5efeec84847
CaptainFearist <igorgolenisev1317@gmail.com>
2025-04-17 01:17:33 +0300

    Load BitmapImage in EmployeeViewModel

--- MVVM/ViewModel/EmployeeViewModel.cs
+++ MVVM/ViewModel/EmployeeViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace File_Manager.MVVM.ViewModel
+﻿using System.Windows.Media.Imaging;
+
+namespace File_Manager.MVVM.ViewModel
 {
     public class EmployeeViewModel
     {
@@ -7,6 +9,31 @@
         public string LastName { get; set; }
         public string Email { get; set; }
         public string DepartmentName { get; set; }
-        public string ImagePath { get; set; }
+        public BitmapImage ProfileImage { get; set; }
+
+        public byte[] ImagePath
+        {
+            get => null;
+            set
+            {
+                if (value != null && value.Length > 0)
+                {
+                    using (var memoryStream = new System.IO.MemoryStream(value))
+                    {
+                        ProfileImage = new BitmapImage();
+                        ProfileImage.BeginInit();
+                        ProfileImage.CacheOption = BitmapCacheOption.OnLoad;
+                        ProfileImage.StreamSource = memoryStream;
+                        ProfileImage.EndInit();
+                        ProfileImage.Freeze();
+                    }
+                }
+                else
+                {
+                    ProfileImage = new BitmapImage(new Uri("pack://application:,,,/Images/default_user.png"));
+                    ProfileImage.Freeze();
+                }
+            }
+        }
     }
 }
